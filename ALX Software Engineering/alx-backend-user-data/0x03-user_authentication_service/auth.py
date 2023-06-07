94a2822aed3b8a35f29acabbe93afb0af2bb5a18
Ian Kisali <iankisali@gmail.com>
2023-06-07 22:57:02 +0300

    generating uuid function

--- ALX Software Engineering/alx-backend-user-data/0x03-user_authentication_service/auth.py
+++ ALX Software Engineering/alx-backend-user-data/0x03-user_authentication_service/auth.py
@@ -3,11 +3,17 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> str:
     """returns bytes."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generating uuids"""
+    return str(uuid4())
 
 
 class Auth:
